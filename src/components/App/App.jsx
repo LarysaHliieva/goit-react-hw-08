0a65e10ebdefe8acebf4eaf9bd6caff6320f6bee
LarysaHliieva <lora.glievaya@gmail.com>
2025-08-12 02:51:24 +0300

    add pending and rejected for auth

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
+
+import CircularProgress from "@mui/material/CircularProgress";
 
 import Loyout from "../Loyout/Loyout";
 import HomePage from "../../pages/HomePage/HomePage";
@@ -11,14 +13,20 @@ import PrivateRoute from "../PrivateRoute/PrivateRoute";
 import RestrictedRoute from "../RestrictedRoute/RestrictedRoute";
 
 import { refreshUser } from "../../redux/auth/operations";
+import { selectIsRefreshing } from "../../redux/auth/selectors";
 
 function App() {
+  const isRefreshing = useSelector(selectIsRefreshing);
+
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
+  return isRefreshing ? (
+    <CircularProgress color="success" />
+  ) : (
     <Loyout>
       <Routes>
         <Route path="/" element={<HomePage />} />
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -2,14 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 import { register, login, logout, refreshUser } from "./operations";
 
-// const handlePending = (state) => {
-//   state.loading = true;
-// };
+const handlePending = (state) => {
+  state.loadingAuth = true;
+};
 
-// const handleRejected = (state, action) => {
-//   state.loading = false;
-//   state.error = action.payload;
-// };
+const handleRejected = (state) => {
+  state.loading = false;
+};
 
 const slice = createSlice({
   name: "auth",
@@ -21,19 +20,28 @@ const slice = createSlice({
     token: null,
     isLoggedIn: false,
     isRefreshing: false,
+    loadingAuth: false,
   },
   extraReducers: (builder) => {
     builder
+      .addCase(register.pending, handlePending)
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
+        state.loadingAuth = false;
       })
+      .addCase(register.rejected, handleRejected)
+
+      .addCase(login.pending, handlePending)
       .addCase(login.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
+        state.loadingAuth = false;
       })
+      .addCase(login.rejected, handleRejected)
+
       .addCase(logout.fulfilled, (state) => {
         state.user = {
           name: null,
@@ -42,9 +50,17 @@ const slice = createSlice({
         state.token = null;
         state.isLoggedIn = false;
       })
+
+      .addCase(refreshUser.pending, (state) => {
+        state.isRefreshing = true;
+      })
       .addCase(refreshUser.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.rejected, (state) => {
+        state.isRefreshing = false;
       });
   },
 });
